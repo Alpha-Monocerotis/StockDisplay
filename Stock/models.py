1afb6ca2128bc38bd03444081d7c8800d29acae0
lmy <1539078757@qq.com>
2019-07-28 09:38:04 +0800

    Revised

--- Stock/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Stock(models.Model):
-
-    name = models.CharField(max_length=150)
-    file = models.CharField(max_length=100)
